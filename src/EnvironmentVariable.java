2ff37da26fc3c8e36f227d178f505fa1f3582690
maaaashin324 <github.upstream352@passmail.net>
2025-03-02 08:27:42 +0000

    feat: retrieving the env from Ssytem package

---
+++ src/EnvironmentVariable.java
@@ -0,0 +1,7 @@
+public class EnvironmentVariable {
+  public static void main(String[] args) {
+    String name = System.getProperty("USERNAME");
+
+    System.out.println("Hello, " + name + "!");
+  }
+}
