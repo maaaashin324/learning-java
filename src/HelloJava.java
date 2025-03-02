b795d2da92c06e767c8c45a57f34ff6762c9d060
maaaashin324 <github.upstream352@passmail.net>
2025-03-02 04:46:23 +0000

    feat: using another class to render the text inside JFrame

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,13 +1,20 @@
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class HelloJava {
   public static void main(String[] args) {
     JFrame frame = new JFrame("Hello, Java!");
-    JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
 
-    frame.add(label);
+    frame.add(new HelloComponent());
     frame.setSize(300, 300);
     frame.setVisible(true);
+  }
+}
+
+class HelloComponent extends JComponent {
+  public void paintComponent(Graphics g) {
+    g.drawString("Hello, Java!", 125, 95);
   }
 }
