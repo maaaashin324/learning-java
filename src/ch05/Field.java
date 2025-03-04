aabeaa3ebca6829fa125d62dec921e0d9e4eed5c
maaaashin324 <github.upstream352@passmail.net>
2025-03-04 06:30:03 +0000

    feat: adding Field class to see how to use methods and class fields that are class instances

---
+++ src/ch05/Field.java
@@ -0,0 +1,29 @@
+package ch05;
+
+public class Field {
+  Apple a1 = new Apple();
+  Apple a2 = new Apple();
+
+  public void setupApples() {
+    a1.diameter = 3.0f;
+    a1.mass = 5.0f;
+    a1.x = 20;
+    a1.y = 40;
+    a2.diameter = 8.0f;
+    a2.mass = 10.0f;
+    a2.x = 70;
+    a2.y = 200;
+  }
+
+  public void detectCollisions() {
+    if (a1.isTouching(a2)) {
+      System.out.println("Collition detected!");
+
+      return;
+    }
+
+    System.out.println("Apples are not touching.");
+
+    return;
+  }
+}
---
+++ src/ch05/PrintAppleDetails4.java
@@ -0,0 +1,15 @@
+package ch05;
+
+class PrintAppleDetails4 {
+  public static void main(String args[]) {
+    Field f = new Field();
+
+    f.setupApples();
+    System.out.println("Apple a1 is : ");
+    f.a1.printDetails();
+    System.out.println("Apple a2 is : ");
+    f.a2.printDetails();
+
+    f.detectCollisions();
+  }
+}
