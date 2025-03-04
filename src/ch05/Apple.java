3f3fd6c8dd9743a83958d29b57ab790d55432855
maaaashin324 <github.upstream352@passmail.net>
2025-03-04 06:15:11 +0000

    feat: adding PrintAppleDetails3 to see how to use the class method

--- src/ch05/Apple.java
+++ src/ch05/Apple.java
@@ -8,4 +8,10 @@ class Apple {
   boolean isTouching(Apple others) {
     return false;
   }
+
+  public void printDetails() {
+    System.out.println("mass of a1 is : " + mass);
+    System.out.println("diameter of a1 is : " + diameter);
+    System.out.println("position of a1 is : (" + x + ", " + y + ")");
+  }
 }
---
+++ src/ch05/PrintAppleDetails3.java
@@ -0,0 +1,17 @@
+package ch05;
+
+public class PrintAppleDetails3 {
+  public static void main(String args[]) {
+    Apple a1 = new Apple();
+
+    System.out.println("Apple a1 is instantiated");
+    a1.printDetails();
+
+    a1.mass = 10.0f;
+    a1.x = 20;
+    a1.y = 42;
+
+    System.out.println("Apple a1 is updated");
+    a1.printDetails();
+  }
+}
