6e73fb105635a2120def218d9a49db67155d99b4
maaaashin324 <github.upstream352@passmail.net>
2025-03-08 09:23:23 +0000

    feat: showing sizes from outside of Apple class

--- src/ch05/examples/PrintAppleDetails.java
+++ src/ch05/examples/PrintAppleDetails.java
@@ -8,5 +8,18 @@ public class PrintAppleDetails {
     System.out.println("mass of a1 is :" + a1.mass);
     System.out.println("diameter of a1 is :" + a1.getDiameter());
     System.out.println("position of a1 is : (" + a1.x + ", " + a1.y + ")");
+
+    String[] sizes = Apple.getAppleSizes();
+
+    if (a1.getDiameter() < 5.0f) {
+      String output = String.format("This Apple is a %s apple", sizes[Apple.SMALL]);
+      System.out.println(output);
+    } else if (a1.getDiameter() < 10.0f) {
+      String output = String.format("This Apple is a %s apple", sizes[Apple.MEDIUM]);
+      System.out.println(output);
+    } else {
+      String output = String.format("This Apple is a %s apple", sizes[Apple.LARGE]);
+      System.out.println(output);
+    }
   }
 }
